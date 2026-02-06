357e6ca35a72f6f15509345e721ccbd3b202390e
Yoon <yoooon0408@gmail.com>
2026-02-06 00:06:45 -0700

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main(){
+    int a, b, c;
+    scanf("%d", &a);
+    for(b=0;b<a;b++){
+        for(c=0;c<b;c++){
+            printf(" ");
+        }
+        for(c=2*a-1;c>=2*b+1;c--){
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    return 0;
+}
