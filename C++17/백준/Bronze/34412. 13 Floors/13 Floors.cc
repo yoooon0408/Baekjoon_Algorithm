258864501076374de9349f0a6122b6318e07bebd
Yoon <yoooon0408@gmail.com>
2026-02-24 13:01:55 -0700

    [Bronze V] Title: 13 Floors, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/34412. 13 Floors/13 Floors.cc
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d", &a);
+    if(a<13){
+        printf("%d", a);
+    }
+    else{
+        printf("%d", a+1);
+    }
+    
+    return 0;
+}
