e4e0be99683a2422db1eeeceb47375f38b48bb79
Yoon <yoooon0408@gmail.com>
2026-02-26 13:01:06 -0700

    [Bronze V] Title: 채플이 너무해, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/34945. 채플이 너무해/채플이 너무해.cc
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a;
+    scanf("%d", &a);
+    if(a>=6){
+        printf("Success!");
+    }
+    else{
+        printf("Oh My God!");
+    }
+    
+    return 0;
+}
