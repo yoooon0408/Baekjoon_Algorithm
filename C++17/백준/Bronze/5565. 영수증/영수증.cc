17e614af38a18d5b267b17bd354e56f8d6c32441
Yoon <yoooon0408@gmail.com>
2026-03-03 13:14:25 -0700

    [Bronze IV] Title: 영수증, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/5565. 영수증/영수증.cc
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+    int sum, a, b, c, d, e, f, g, h, i, j;
+    scanf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d", &sum, &a, &b, &c, &d, &e, &f, &g, &h, &i, &j);
+    printf("%d",sum-(a+b+c+d+e+f+g+h+i));
+        
+    return 0;
+}
