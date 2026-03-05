67d35259744fcd670b24669149d3113a979efe68
Yoon <yoooon0408@gmail.com>
2026-03-05 12:09:06 -0700

    [Bronze IV] Title: 이 대회는 이제 제 겁니다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/31922. 이 대회는 이제 제 겁니다/이 대회는 이제 제 겁니다.cc
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+     if(a+c>=b){
+         printf("%d", a+c);
+     }
+    else if(a+c<b){
+        printf("%d", b);
+    }
+
+    return 0;
+}
